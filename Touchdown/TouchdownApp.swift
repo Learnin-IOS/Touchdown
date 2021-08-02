90020b30dd8a4a6bebe3692b3319b946b1e007e8
Le Bon B Bauma <baumabulondo@gmail.com>
2021-08-02 10:34:39 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Le Bon B' Bauma on 02/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
