3b0ee0ed12a155dc704123cc3f7bb2c97354e87b
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-08-06 07:03:30 -0700

    Constant	Utility

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Le Bon B' Bauma on 06/08/2021.
+//
+
+import SwiftUI
+
+// DATA
+let colorBackground: Color = Color("ColorBackground")
+
+// COLOR
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
