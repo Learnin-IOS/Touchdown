58c91e7b480c4a50790194c35e49d05bebf15a7d
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-08-07 16:25:36 -0700

    Content View

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Properties
+
+    // MARK: - Body
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack (spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            }//: VSATCK
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        }//: ZSTACK
+        .ignoresSafeArea(.all, edges: .top )
     }
 }
+// MARK: - Preview
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
