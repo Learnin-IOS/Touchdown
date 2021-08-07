5b81daf7fc3b25a129f9995404c5b6add034e7a8
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-08-07 16:24:17 -0700

    LogoView

---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,44 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Le Bon B' Bauma on 07/08/2021.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    // MARK: - Properties
+    
+    
+    // MARK: - Body
+
+    var body: some View {
+        HStack(spacing: 4){
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        }//: HSTACK
+    }
+}
+// MARK: - Preview
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
