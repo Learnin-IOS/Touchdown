f469bba276f69b57e62c27b54ad19fca1289f186
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-08-07 17:21:10 -0700

    Player Model

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Le Bon B' Bauma on 07/08/2021.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
