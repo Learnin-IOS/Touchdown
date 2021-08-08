5a96607e4900b6d4aacbe947169888d19025c617
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-08-07 17:21:31 -0700

    Constant

--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 // DATA
 
+let players: [Player] = Bundle.main.decode("player.json")
+
+// COLOR
 let colorBackground: Color = Color("ColorBackground")
 let colorGray: Color = Color(UIColor.systemGray4)
 
-// COLOR
+
 // LAYOUT
 // UX
 // API
