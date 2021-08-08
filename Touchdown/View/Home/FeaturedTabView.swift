2abbcf049ee7eece753496f2b7eeb370e5bd509f
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-08-07 17:20:42 -0700

    FeaturedTab View

---
+++ Touchdown/View/Home/FeaturedTabView.swift
@@ -0,0 +1,35 @@
+//
+//  FeaturedTabView.swift
+//  Touchdown
+//
+//  Created by Le Bon B' Bauma on 07/08/2021.
+//
+
+import SwiftUI
+
+struct FeaturedTabView: View {
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Body
+
+    var body: some View {
+        TabView {
+            ForEach(players) { player in
+                FeaturedItemView(player: player)
+                    .padding(.top, 10)
+                    .padding(.horizontal, 15)
+            }//: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+    }
+}
+// MARK: - Preview
+
+struct FeaturedTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedTabView()
+            .background(Color.gray)
+    }
+}
