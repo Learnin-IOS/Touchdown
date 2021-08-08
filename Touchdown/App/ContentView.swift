6ee95a9e64717978590aa17ea786349f1c49f8a9
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-08-07 17:19:57 -0700

    Application

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -22,10 +22,17 @@ struct ContentView: View {
                     .background(Color.white)
                     .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
                 
-                Spacer()
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    VStack(spacing: 0){
+                        FeaturedTabView()
+                            .padding(.vertical, 20)
+                        
+                        FooterView()
+                            .padding(.horizontal)
+                    } //: VSTACK
+                })//: SCROLL
                 
-                FooterView()
-                    .padding(.horizontal)
+                
             }//: VSATCK
             .background(colorBackground.ignoresSafeArea(.all, edges: .all))
         }//: ZSTACK
