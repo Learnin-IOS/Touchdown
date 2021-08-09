c91e33a0af4e1537572ecbdeae5f137c8fb2b214
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-08-09 05:21:36 -0700

    Content View

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -21,17 +21,19 @@ struct ContentView: View {
                     .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
                     .background(Color.white)
                     .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
-                
-                ScrollView(.vertical, showsIndicators: false, content: {
-                    VStack(spacing: 0){
+                ScrollView(.vertical, showsIndicators: false, content:{
+                    VStack(spacing: 0) {
+                        
                         FeaturedTabView()
                             .padding(.vertical, 20)
                         
                         FooterView()
                             .padding(.horizontal)
+
                     } //: VSTACK
-                })//: SCROLL
-                
+                    
+                    
+                }) // : SCROLL
                 
             }//: VSATCK
             .background(colorBackground.ignoresSafeArea(.all, edges: .all))
@@ -44,5 +46,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro")
     }
 }
