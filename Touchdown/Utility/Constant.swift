ac739b365be297754c480c6de686c004bc6f25b4
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-08-13 02:38:18 -0700

    Refactoring

--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -12,6 +12,7 @@ import SwiftUI
 let players: [Player] = Bundle.main.decode("player.json")
 
 // COLOR
+
 let colorBackground: Color = Color("ColorBackground")
 let colorGray: Color = Color(UIColor.systemGray4)
 
