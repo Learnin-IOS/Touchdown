a0e565900dba739d77d0680ad95be7e7007005e3
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-08-13 02:38:31 -0700

    Refactoring

--- Touchdown/Extension/CodableBundleExtension.swift
+++ Touchdown/Extension/CodableBundleExtension.swift
@@ -9,6 +9,7 @@ import Foundation
 
 extension Bundle {
     func decode<T: Codable>(_ file: String) -> T {
+        
         // 1. Locate the JSON File
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle.")
@@ -19,6 +20,7 @@ extension Bundle {
         }
         // 3. Create a decoder
         let decoder = JSONDecoder()
+    
         
         // 4. Create a property for the decoded data
         guard let decodedData = try? decoder.decode(T.self, from: data) else {
