35323ceb1fb7c5782d20a5829e0ca279023863fe
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-08-14 12:44:41 -0700

    Refactoring

--- Touchdown/View/Home/FooterView.swift
+++ Touchdown/View/Home/FooterView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct FooterView: View {
+    
     var body: some View {
         VStack(alignment: .center, spacing: 10) {
             Text("We offer the most cutting edge, confortable, lightweight and durable football helmets in the market at afordable prices.")
