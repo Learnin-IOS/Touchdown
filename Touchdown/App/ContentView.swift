39e742f1861cbd5d8519f2cbdcfac7a1c02096c4
lebonbbauma <baumabulondo@gmail.com>
2021-11-24 19:21:26 +0300

    refactor: Featured Tab View

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -22,14 +22,15 @@ struct ContentView: View {
                     .background(Color.white)
                     .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
                 ScrollView(.vertical, showsIndicators: false, content:{
-                    VStack(spacing: 0) {
+                    VStack {
                         
                         FeaturedTabView()
-                            .padding(.vertical, 20)
+                            
+                        Spacer()
                         
                         FooterView()
                             .padding(.horizontal)
-
+  
                     } //: VSTACK
                     
                 }) // : SCROLL
