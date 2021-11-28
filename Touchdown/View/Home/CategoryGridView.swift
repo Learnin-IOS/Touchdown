1587b8dd715fa2cdd335ced7c213ca1f2e139e23
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-11-29 00:57:55 +0300

    feat: Implementing category lazy grid layout

---
+++ Touchdown/View/Home/CategoryGridView.swift
@@ -0,0 +1,37 @@
+//
+//  CategoryGridView.swift
+//  Touchdown
+//
+//  Created by Le Bon B' Bauma on 29/11/2021.
+//
+
+import SwiftUI
+
+struct CategoryGridView: View {
+    // MARK: Property
+    
+    // MARK: Body
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHGrid(rows: gridLayout, alignment: .center, spacing: columnSpacing, pinnedViews: [], content: {
+                
+                ForEach(categories) { category in CategoryItemView(category: category)
+                    
+                }
+            }) //:  GRID
+        }) //: SCROLL
+            .frame(height: 140)
+            .padding(.horizontal, 15)
+            .padding(.vertical, 10)
+         
+    }
+}
+// MARK: Preview
+struct CategoryGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryGridView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(colorBackground)
+    }
+}
