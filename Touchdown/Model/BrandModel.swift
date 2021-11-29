d035b91564fe5a79b0e9ccf53ef6ef70fac12922
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-11-29 17:43:45 +0300

    build: Initialising Brand data model

---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Le Bon B' Bauma on 29/11/2021.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
