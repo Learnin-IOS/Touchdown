2adefce1a59a084c86621c3193f9f56c08165891
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-11-29 11:58:57 +0300

    feat: Implementing Vertical grid layout for products

--- Touchdown/Model/ProductModel.swift
+++ Touchdown/Model/ProductModel.swift
@@ -15,9 +15,9 @@ struct Product: Codable, Identifiable {
     let description: String
     let color: [Double]
     
-//    var red: Double{return color[0]}
-//    var blue: Double{return color[1]}
-//    var green: Double{return color[2]}
-//
-//    var formattedprice: String
+    var red: Double{return color[0]}
+    var green: Double{return color[1]}
+    var blue: Double{return color[2]}
+
+    var formattedprice: String {return "$\(price)"}
 }
--- Touchdown/View/Home/ProductItemView.swift
+++ Touchdown/View/Home/ProductItemView.swift
@@ -23,7 +23,7 @@ struct ProductItemView: View {
                     .scaledToFit()
                     .padding(10)
             }
-            .background(Color(red: product.color[0], green: product.color[1], blue: product.color[2]))
+            .background(Color(red: product.red, green: product.green, blue: product.blue))
             .cornerRadius(12)
             
              // NAME
@@ -32,7 +32,7 @@ struct ProductItemView: View {
                 .fontWeight(.black)
             
             // PRICE
-            Text("$\(product.price )")
+            Text(product.formattedprice)
                 .fontWeight(.semibold)
                 .foregroundColor(.gray)
             
