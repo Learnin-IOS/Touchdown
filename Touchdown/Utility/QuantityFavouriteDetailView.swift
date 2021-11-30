083526a2c4f7f9468ba754a00a35b43e491d2eb4
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-11-30 13:00:10 +0300

    feat : Adding Quantity and favourite options to the product description

---
+++ Touchdown/Utility/QuantityFavouriteDetailView.swift
@@ -0,0 +1,57 @@
+//
+//  QuantityFavouriteDetailView.swift
+//  Touchdown
+//
+//  Created by Le Bon B' Bauma on 30/11/2021.
+//
+
+import SwiftUI
+
+struct QuantityFavouriteDetailView: View {
+    
+    // MARK: - Property
+    
+    @State private var counter: Int = 0
+    // MARK: - Body
+    var body: some View {
+        HStack(alignment: .center, spacing: 6, content: {
+            Button(action: {
+                if counter > 0 {
+                    counter -= 1
+                }
+            }, label: {
+                Image(systemName: "minus.circle")
+            })//: BUTTON
+            
+            Text("\(counter)")
+                .fontWeight(.semibold)
+                .frame(minWidth: 36)
+            
+            Button(action: {
+                if counter < 100 {
+                    counter += 1
+                }
+            }, label: {
+                Image(systemName: "plus.circle")
+            })//: BUTTON
+            Spacer()
+            
+            Button(action: {}, label: {
+                Image(systemName: "heart.circle")
+                    .foregroundColor(.pink)
+            })//: BUTTON
+        })//: HSTACK
+            .font(.system(.title, design: .rounded ))
+            .foregroundColor(.black)
+            .imageScale(.large)
+    }
+}
+
+// MARK: - Preview
+struct QuantityFavouriteDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuantityFavouriteDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
