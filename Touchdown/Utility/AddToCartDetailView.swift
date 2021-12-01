254fa90a5562fefc0a33537c75c24e0291af10ff
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-12-01 23:52:46 +0300

    feat : Add to cart View

--- Touchdown/Utility/AddToCartDetailView.swift
+++ Touchdown/Utility/AddToCartDetailView.swift
@@ -11,12 +11,14 @@ struct AddToCartDetailView: View {
     
     // MARK: - Property
     
-    
+    @EnvironmentObject var shop: Shop
     
     // MARK: - Body
     
     var body: some View {
-        Button(action: {}, label: {
+        Button(action: {
+            feedback.impactOccurred() 
+        }, label: {
             Spacer()
             Text("Add to cart".uppercased())
                 .font(.system(.title2, design: .rounded))
@@ -26,9 +28,9 @@ struct AddToCartDetailView: View {
         }) //: BUTTON
             .padding()
             .background(
-                Color(red: sampleProduct.red,
-                      green: sampleProduct.green,
-                      blue: sampleProduct.blue)
+                Color(red: shop.selectedProduct?.red ?? sampleProduct.red,
+                      green: shop.selectedProduct?.green ?? sampleProduct.green,
+                      blue: shop.selectedProduct?.blue ?? sampleProduct.blue)
             )
             .clipShape(Capsule())
     }
@@ -38,6 +40,7 @@ struct AddToCartDetailView: View {
 struct AddToCartDetailView_Previews: PreviewProvider {
     static var previews: some View {
         AddToCartDetailView()
+            .environmentObject(Shop())
             .previewLayout(.sizeThatFits)
             .padding()
     }
