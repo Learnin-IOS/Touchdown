2534f25c8f1fd8f0ab072fb442d1266ca9b734f1
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-12-02 23:46:39 +0300

    Refactor

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -9,51 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - Properties
-
+    @ EnvironmentObject var shop: Shop
     // MARK: - Body
-
+    
     var body: some View {
         ZStack {
-            VStack (spacing: 0) {
-                NavigationBarView()
-                    .padding(.horizontal, 15)
-                    .padding(.bottom)
-                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
-                    .background(Color.white)
-                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
-                 ScrollView(.vertical, showsIndicators: false, content:{
-                    VStack { 
-                        
-                        FeaturedTabView()
-                            .frame(width: UIScreen.main.bounds.width - 50, height: 210)
-                            .padding(.vertical, 20)
-                        CategoryGridView()
-                        
-                        TitleView(title: "Helmets")
-                        
-                        LazyVGrid(columns: gridLayout,spacing: 15, content: {
-                            ForEach(products) { product in
-                                ProductItemView(product: product)
-                            } //: LOOP
+            if shop.showingProduct == false && shop.selectedProduct == nil {
+                VStack (spacing: 0) {
+                    NavigationBarView()
+                        .padding(.horizontal, 15)
+                        .padding(.bottom)
+                        .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                        .background(Color.white)
+                        .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                    ScrollView(.vertical, showsIndicators: false, content:{
+                        VStack {
                             
-                        }) //: GRID
-                            .padding(15)
-                        
-                        TitleView(title: "Brands")
-                        
-                        BrandGridView()
-                        
-                        FooterView()
-                            .padding(.horizontal)
+                            FeaturedTabView()
+                                .frame(width: UIScreen.main.bounds.width - 50, height: 210)
+                                .padding(.vertical, 20)
+                            CategoryGridView()
                             
-  
-                    } //: VSTACK
-                    
-                }) // : SCROLL
-                
-            }//: VSATCK
-            
-            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+                            TitleView(title: "Helmets")
+                            
+                            LazyVGrid(columns: gridLayout,spacing: 15, content: {
+                                ForEach(products) { product in
+                                    ProductItemView(product: product)
+                                        .onTapGesture {
+                                            feedback.impactOccurred()
+                                            withAnimation(.easeOut){
+                                                shop.selectedProduct = product
+                                                shop.showingProduct = true
+                                                
+                                            }
+                                        }
+                                } //: LOOP
+                                
+                            }) //: GRID
+                                .padding(15)
+                            
+                            TitleView(title: "Brands")
+                            
+                            BrandGridView()
+                            
+                            FooterView()
+                                .padding(.horizontal)
+                        } //: VSTACK
+                    }) // : SCROLL
+                }//: VSATCK
+                .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+            } else {
+                 ProductDetailView()
+            }
         }//: ZSTACK
         .ignoresSafeArea(.all, edges: .top )
     }
@@ -63,6 +70,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-//            .previewDevice("iPhone 12 Pro")
+            .environmentObject(Shop())
+        //            .previewDevice("iPhone 12 Pro")
     }
 }
