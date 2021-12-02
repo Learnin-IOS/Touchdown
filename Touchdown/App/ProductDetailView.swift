5747d8b016f4486c7fa7ecb1480179522bb8153d
Le Bon B' Bauma <baumabulondo@gmail.com>
2021-12-02 23:46:23 +0300

    fix: Product detail view

--- Touchdown/App/ProductDetailView.swift
+++ Touchdown/App/ProductDetailView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct ProductDetailView: View {
     
+    // MARK: - Property
+    @EnvironmentObject var shop: Shop
+    
+    // MARK: - Body
     var body: some View {
         VStack(alignment: .leading, spacing: 5, content: {
             // NAVBAR
@@ -31,7 +35,7 @@ struct ProductDetailView: View {
                     .padding(.bottom, 10)
                 // DESCRIPTION
                 ScrollView(.vertical, showsIndicators: false, content: {
-                    Text(sampleProduct.description)
+                    Text(shop.selectedProduct?.description ?? sampleProduct.description)
                         .font(.system(.body, design: .rounded))
                         .foregroundColor(.gray)
                         .multilineTextAlignment(.leading)
@@ -55,18 +59,20 @@ struct ProductDetailView: View {
             .zIndex(0)
             .ignoresSafeArea(.all, edges: .all)
         background(
-            Color(red: sampleProduct.red,
-                  green: sampleProduct.green,
-                  blue: sampleProduct.blue
+            Color(red: shop.selectedProduct?.red ?? sampleProduct.red,
+                  green: shop.selectedProduct?.green ?? sampleProduct.green,
+                  blue: shop.selectedProduct?.blue ?? sampleProduct.blue
                  ).ignoresSafeArea(.all, edges: .all)
         )
         
     }
 }
 
+// MARK: - Preview
 struct ProductDetailView_Previews: PreviewProvider {
     static var previews: some View {
         ProductDetailView()
             .previewLayout(.fixed(width: 375, height: 812))
+            .environmentObject(Shop())
     }
 }
